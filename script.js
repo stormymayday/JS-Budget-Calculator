0ba43ed4c743c1520b4288dee25b5527dc665922
stormymayday <stormymayday7@gmail.com>
2020-06-12 17:34:07 -0600

    Added the DOMstrings object inside the UIController to hold CSS class names of the input elements and a public method getDOMstrings to access it. Added another public method inside the UIController that reads data from the input elements.

--- script.js
+++ script.js
@@ -6,15 +6,61 @@ var budgetController = (function () {
 
 var UIController = (function () {
 
-    // Some code
+    // Private
+
+    var DOMstrings = {
+
+        // Input class names
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+
+        // The input 'add' button class name
+        inputBtn: '.add__btn'
+
+    };
+
+    // Public
+    return {
+
+        getInput: function () {
+
+            return {
+
+                // Reading data from the dropdown menu. Value can be 'inc' or 'exp'.
+                type: document.querySelector(DOMstrings.inputType).value,
+
+                // Reading data from the 'description' input field
+                description: document.querySelector(DOMstrings.inputDescription).value,
+
+                // Reading data from  the 'value' input field
+                value: document.querySelector(DOMstrings.inputValue).value
+
+            };
+
+        },
+
+        getDOMstrings: function () {
+
+            return DOMstrings;
+
+        }
+
+    };
+
+
 
 })();
 
 var appController = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function () {
 
         // 1. Get data from the input field
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // 2. Add the item to the budgetController
 
@@ -28,7 +74,7 @@ var appController = (function (budgetCtrl, UICtrl) {
 
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
 
