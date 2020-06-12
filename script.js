95e2a5239f322848f4eb90e7d1b066af03205b29
stormymayday <stormymayday7@gmail.com>
2020-06-12 16:02:48 -0600

    Added Event Listener to the input button ('.add__btn') and to the Enter keypess.

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 var budgetController = (function () {
 
-    var a = 2;
-
-    var addA = function (arg) {
-        return arg + a;
-    };
-
-    return {
-        publicAdd: function (arg) {
-            return addA(arg);
-        }
-    }
+    // Some code
 
 })();
 
 var UIController = (function () {
 
-    // Some Code
+    // Some code
 
 })();
 
 var appController = (function (budgetCtrl, UICtrl) {
 
-    var testVar = budgetCtrl.publicAdd(5);
+    var ctrlAddItem = function () {
 
-    return {
-        publicTest: function () {
+        // 1. Get data from the input field
 
-            return testVar;
+        // 2. Add the item to the budgetController
+
+        // 3. Addd the to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        console.log('Hi');
+
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+
+            ctrlAddItem();
 
         }
-    }
+
+    });
 
 })(budgetController, UIController);
-
-
-console.log(appController.publicTest());
