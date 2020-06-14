00e8fc360800fd0ea13b873ccae744024894598d
stormymayday <stormymayday7@gmail.com>
2020-06-13 20:36:06 -0600

    Created a new public method inside the UIController module called displayBudget that displays the budget, total income, and expenses in the appropriate HTML elements.

--- script.js
+++ script.js
@@ -153,7 +153,13 @@ var UIController = (function () {
 
         // Income and Expenses lists
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+
+        // Labels for total Budget, Income, Expenses, and Expenses percentage
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
 
     };
 
@@ -232,6 +238,23 @@ var UIController = (function () {
             // Setting focus on the first input field (description)
             fieldsArray[0].focus();
 
+        },
+
+        displayBudget: function (obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+
+            } else {
+
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+
+            }
         }
 
     };
@@ -271,6 +294,7 @@ var appController = (function (budgetCtrl, UICtrl) {
 
         // 2. Displaying the budget on the UI
         console.log(budget);
+        UICtrl.displayBudget(budget);
 
     };
 
@@ -309,6 +333,14 @@ var appController = (function (budgetCtrl, UICtrl) {
 
             // test
             console.log('The app has been initialized.');
+
+            // Clearing the labels
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
 
             // Setting up the Event Listeners
             setupEventListeners();
